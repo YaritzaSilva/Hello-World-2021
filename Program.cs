07e8be03198507d3023a4187e714bff3a1cb996b
Yaritza <yaritzamicaelleb@gmail.com>
2021-03-01 13:35:00 -0300

    Primeira versão do Programa

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello_World_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+         
+        }
+    }
+}
